f9b878aaeae5f91ddb6833b92c1750177d254b72
Roger Chen <rogerthechen@gmail.com>
2012-11-07 23:25:52 -0800

    Initial Commit

---
+++ Sirona/SironaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SironaAppDelegate.h
+//  Sirona
+//
+//  Created by Roger Chen on 11/7/12.
+//  Copyright (c) 2012 Roger Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SironaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sirona/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sirona
+//
+//  Created by Roger Chen on 11/7/12.
+//  Copyright (c) 2012 Roger Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SironaAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SironaAppDelegate class]));
+    }
+}
